0f2bfbb387878a943d6792b644290685dae6b479
Takahito Yamada <d_p.t.a.d.fighter@icloud.com>
2020-09-21 10:49:41 +0900

    add finish swift playground

--- chap2/MyPlayground.playground/Contents.swift
+++ chap2/MyPlayground.playground/Contents.swift
@@ -121,3 +121,114 @@ func requestMinAndMax() -> (min: Int, max: Int) {
 let minAndMax = requestMinAndMax()
 let minValue = minAndMax.min
 let maxValue = minAndMax.max
+
+for index in 0...9 {
+    print("index \(index)")
+}
+
+let valuesFruit = ["apple", "gorilla", "rakuda"]
+for val in valuesFruit {
+    print(val)
+}
+
+for index in 0..<9 {
+    print("index \(index)")
+}
+
+for _ in 0...9 {
+    print("index")
+}
+
+for (idx, val) in valuesFruit.enumerated() {
+    print("\(idx): \(val)")
+}
+
+let val5 = 10
+if val5 > 10 {
+    print("value is more than 10")
+} else if val5 == 10 {
+    print("value is 10")
+}else {
+    print("value is less than 10")
+}
+
+
+let val6 = 100
+switch val6 {
+case 0:
+    print("value is 0")
+case 1...100:
+    print("value is 1 ~ 100")
+default:
+    print("more than 100")
+}
+
+let val7 = "apple"
+switch val7 {
+case "apple":
+    print("this is apple")
+    fallthrough
+case "orange":
+    print("this is ornage")
+default:
+    print("unknown")
+}
+
+var count = 1
+var result = 0
+while count < 10 {
+    result += count
+    count += 1
+}
+print("summurize is \(result)")
+
+
+func buyItem(myMoney: Int?){
+    guard let money = myMoney else {
+        return
+    }
+    
+    print("use \(money)")
+}
+//
+//let reulsts = [10, 4, 25, 16, 5].filter{ (x: Int) -> Bool in
+//    if x < 10 {
+//        return true
+//    }
+//    return false
+//}
+
+
+var arr = [10, 4, 25, 16, 5]
+let reulsts = arr.filter{$0 < 10}
+
+print(reulsts)
+
+
+print(arr.map{ $0 * 2 })
+
+print(arr.reduce(0) { $0 + $1})
+print(arr.sorted{ $0 < $1 })
+
+
+for val in 0..<9 where val != 5 {
+    print(val)
+}
+
+let val8 = "orrange"
+switch val8{
+case let x where x.hasPrefix("o"):
+    print("this is orange")
+case "apple":
+    print("this is apple")
+default:
+    print("unknown")
+}
+
+enum Fruit2 {
+    case Apple, Orange
+}
+let fruit2 :[Fruit2] = [.Apple, .Orange, .Apple, .Orange]
+for case .Apple in fruit2 {
+    print("this is apple")
+}
